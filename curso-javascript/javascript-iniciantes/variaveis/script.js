a57869f9baf41fb221461e2e2f178e6213720331
Victor Siqueira Ribeiro <victor.ribeiro@comlink.com.br>
2022-11-02 10:18:37 -0300

    Inicializando repositório e enviando arquivos das aulas iniciais

---
+++ curso-javascript/javascript-iniciantes/variaveis/script.js
@@ -0,0 +1,27 @@
+var nome = 'Victor';
+var idade = 26;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+//jeito tradicional de criar variáveis
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+//outra forma de declarar variável (sem repetir a palavra VAR)
+var preco = 25, totalComprado = 5, totalPreco = totalComprado * preco;
+
+/* EXERCÍCIO 1 */
+//Declarar uma variável com o seu nome
+var meuNome = 'Victor';
+//Declarar uma variável com a sua idade
+var minhaIdade = 26;
+//Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita = '';
+//Atribuir valor à sua comida favorita
+var comidaFavorita = 'Comida japonesa';
+//Declarar 5 variáveis diferentes sem valores
+var var1, var2, var3, var4, var5;
+
+console.log(comidaFavorita);
