451660bba97dc154f75cbfa1643e598a1962277b
Victor Siqueira Ribeiro <victor.ribeiro@comlink.com.br>
2022-11-02 11:08:06 -0300

    Continuando curso..

---
+++ curso-javascript/javascript-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,38 @@
+
+
+
+var nome = 'Victor';
+var sobrenome = 'Ribeiro';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+var ano = '2018'
+var mes = 08;
+console.log(ano + mes);
+
+var frase1 = 'Esse é o \"melhor\" jogo';
+console.log(frase1);
+
+
+//template string
+var gols = 1000;
+var frase2 = 'Ronaldo fez ' + gols + ' gols no jogo';
+console.log(frase2);
+var frase3 = `Ronaldo fez ${gols} gols`;
+console.log(frase3);
+
+//EXERCÍCIOS
+// Declare uma variável contendo uma string
+    var variavel1 = 'Olá';
+// Declare uma variável contendo um número dentro de uma string
+    var variavel2 = 'Eu tenho 26 anos';
+// Declare uma variável com a sua idade
+    var variavel3 = 26;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+    var nome2 = 'Victor', sobrenome2 = 'Ribeiro' 
+    console.log(nome2 + sobrenome2);
+// Coloque a seguinte frase em uma variável: It's time
+    var frase4 = "It's time"
+// Verifique o tipo da variável que contém o seu nome
+    console.log(typeof nome2)
